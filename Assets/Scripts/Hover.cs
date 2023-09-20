940e7f25dd2f5c24cc56120d676a75b85e72ad19
Eliksni <Ipodmaster2000@gmail.com>
2023-09-19 18:26:55 -0700

    Added Hover effect

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    public float amplitude = 0.1f;
+    public float frequency = 3f;
+
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = startPos + Vector3.up * amplitude * Mathf.Sin(Time.time * frequency);
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# IMTC 505 Lab 1
+
+Within this lab we were tasked with creating using the provided unitypackage to create a mini game with some added features and additional assets. Specifically:
+ - At least three additional assets/models/images included in the scene
+ - Two different behaivors
+ - Behaviors added to all point objects
+ - Behaviors added to at least 3 environment objects
+
+The
